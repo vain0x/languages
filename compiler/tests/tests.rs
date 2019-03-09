c17475ef9a4bbaa4c8b0a3a005e53383330a3d9c
vain0x <vainzerox@gmail.com>
2019-03-10 02:20:25 +0900

    refactor: Solve "Welcome to AtCoder"

--- compiler/tests/tests.rs
+++ compiler/tests/tests.rs
@@ -331,49 +331,81 @@ fn test_fun_to_modify_globals() {
     )
 }
 
+static STDLIB: &str = r#"
+    let DIGIT_CHARS = "0123456789";
+    let HYPHEN_CHAR = "-"[0];
+    let calculate_digit_len = fun(x) {
+        if x == 0 {
+            1
+        } else {
+            let len = 0;
+            if x < 0 {
+                x = -x;
+                len += 1;
+            }
+            while x > 0 {
+                len += 1;
+                x = x / 10;
+            }
+            len
+        }
+    };
+    let int_to_str = fun(x) {
+        let n = calculate_digit_len(x);
+        let s = mem_alloc(n);
+        if x == 0 {
+            s[0] = DIGIT_CHARS[0];
+        } else {
+            if x < 0 {
+                s[0] = HYPHEN_CHAR;
+                x = -x;
+            }
+            while x > 0 {
+                n = n - 1;
+                s[n] = DIGIT_CHARS[x % 10];
+                x = x / 10;
+            }
+        }
+        s
+    };
+"#;
+
 #[test]
 fn test_int_to_str() {
     eval_tests(
-        r#"
-            let DIGIT_CHARS = "0123456789";
-            let HYPHEN_CHAR = "-"[0];
-            let calculate_digit_len = fun(x) {
-                if x == 0 {
-                    1
-                } else {
-                    let len = 0;
-                    if x < 0 {
-                        x = -x;
-                        len += 1;
-                    }
-                    while x > 0 {
-                        len += 1;
-                        x = x / 10;
-                    }
-                    len
-                }
-            };
-            let int_to_str = fun(x) {
-                let n = calculate_digit_len(x);
-                let s = mem_alloc(n);
-                if x == 0 {
-                    s[0] = DIGIT_CHARS[0];
-                } else {
-                    if x < 0 {
-                        s[0] = HYPHEN_CHAR;
-                        x = -x;
-                    }
-                    while x > 0 {
-                        n = n - 1;
-                        s[n] = DIGIT_CHARS[x % 10];
-                        x = x / 10;
-                    }
-                }
-                s
-            };
+        &format!(
+            r#"
+            {}
             print(int_to_str(read_int()));
             print("\n");
         "#,
+            STDLIB
+        ),
         &[("314159", "314159\n"), ("0", "0\n"), ("-42", "-42\n")],
+    )
+}
+
+/// <https://atcoder.jp/contests/abs/tasks/practice_1?lang=en>
+#[test]
+fn test_welcome_to_atcoder() {
+    eval_tests(
+        &format!(
+            r#"
+            {}
+            let a = read_int();
+            let b = read_int();
+            let c = read_int();
+            let s = read_str();
+            print(int_to_str(a + b + c));
+            print(" ");
+            print(s);
+            print("\n");
+        "#,
+            STDLIB
+        ),
+        &[
+            ("1\n2 3\ntest\n", "6 test\n"),
+            ("72\n128 256\nmyonmyon\n", "456 myonmyon\n"),
+        ],
     )
 }

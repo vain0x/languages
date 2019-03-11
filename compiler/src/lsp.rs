18a0a63be0529f63091441c291edbd0701d23035
vain0x <vainzerox@gmail.com>
2019-03-12 01:39:19 +0900

    feat: Set up log

--- compiler/src/lsp.rs
+++ compiler/src/lsp.rs
@@ -10,7 +10,18 @@ pub(self) use lsp_receiver::LspReceiver;
 pub(self) use lsp_sender::LspSender;
 use std::io;
 
+fn init_log() {
+    let log_level = if cfg!(debug_assertions) {
+        log::LevelFilter::Debug
+    } else {
+        log::LevelFilter::Warn
+    };
+    env_logger::Builder::new().filter(None, log_level).init();
+}
+
 pub fn start_lsp_server() {
+    init_log();
+
     let stdin = io::stdin();
     let stdin = stdin.lock();
     let receiver = LspReceiver::new(stdin);

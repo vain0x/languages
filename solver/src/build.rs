b279d65da0260cefe5645dc2a925c128aefec144
vain0x <vainzerox@gmail.com>
2019-03-17 20:18:14 +0900

    refactor: Reduce verbosity

--- solver/src/build.rs
+++ solver/src/build.rs
@@ -11,10 +11,7 @@ use picomet_lang_compiler::gen_rust::{gen_rust_to_file};
 fn main() {
     let root_path = env!("CARGO_MANIFEST_DIR");
     let runtime_path = Path::new(root_path)
-        .join("..")
-        .join("runtime")
-        .join("src")
-        .join("lib.rs");
+        .join("../runtime/src/lib.rs");
     let picomet_path = Path::new(root_path).join("src").join("main.picomet");
     let out_path = Path::new(root_path).join("src").join("main.rs");
 

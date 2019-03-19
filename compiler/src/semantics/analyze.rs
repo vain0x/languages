9aa79af3ca64220843298166da3a6b0f8b931158
vain0x <vainzerox@gmail.com>
2019-03-20 00:25:48 +0900

    fix: Fix subst_ty to substitute type recursively

--- compiler/src/semantics/analyze.rs
+++ compiler/src/semantics/analyze.rs
@@ -491,26 +491,35 @@ impl Sema {
     }
 
     pub fn get_ty(&self, exp_id: ExpId) -> Ty {
-        self.subst_ty(&Ty::Var(exp_id)).to_owned()
+        self.subst_ty(&Ty::Var(exp_id))
     }
 
     fn bind_ty(&mut self, exp_id: ExpId, ty: &Ty) {
-        self.exp_tys.insert(exp_id, ty.to_owned());
+        self.exp_tys.borrow_mut().insert(exp_id, ty.to_owned());
     }
 
-    fn subst_ty<'a>(&'a self, ty: &'a Ty) -> &'a Ty {
+    fn subst_ty(&self, ty: &Ty) -> Ty {
         match ty {
-            Ty::Var(exp_id) => match self.exp_tys.get(&exp_id) {
-                Some(ty) => ty,
-                None => ty,
-            },
-            _ => ty,
+            &Ty::Var(exp_id) => {
+                let next = {
+                    match self.exp_tys.borrow().get(&exp_id) {
+                        None => return ty.clone(),
+                        Some(&Ty::Var(x)) if x == exp_id => return ty.clone(),
+                        Some(next) => next.clone(),
+                    }
+                };
+
+                let ty = self.subst_ty(&next);
+                self.exp_tys.borrow_mut().insert(exp_id, ty.clone());
+                ty
+            }
+            _ => ty.clone(),
         }
     }
 
     fn unify_ty(&mut self, exp_id: ExpId, l_ty: &Ty, r_ty: &Ty) {
-        let subst_l_ty = self.subst_ty(l_ty).to_owned();
-        let subst_r_ty = self.subst_ty(r_ty).to_owned();
+        let subst_l_ty = self.subst_ty(l_ty);
+        let subst_r_ty = self.subst_ty(r_ty);
         match (&subst_l_ty, &subst_r_ty) {
             (Ty::Var(l_exp_id), Ty::Var(r_exp_id)) if l_exp_id == r_exp_id => {}
             (&Ty::Var(exp_id), _) | (_, &Ty::Var(exp_id)) => self.bind_ty(exp_id, &subst_r_ty),
@@ -558,7 +567,7 @@ pub(crate) fn sema(syntax: Rc<Syntax>) -> Sema {
             exp_vals: BTreeSet::new(),
             exp_ranges: BTreeMap::new(),
             exp_decls: BTreeSet::new(),
-            exp_tys: BTreeMap::new(),
+            exp_tys: RefCell::new(BTreeMap::new()),
             exp_parent: BTreeMap::new(),
             vars: BTreeMap::new(),
             funs: BTreeMap::new(),

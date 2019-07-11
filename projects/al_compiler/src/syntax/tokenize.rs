45582a394c056440cc2ae432e2ced78ec566ae86
vain0x <vainzerox@gmail.com>
2019-07-11 23:33:55 +0900

    refactor: Tokenizer をジェネリックにする

--- projects/al_compiler/src/syntax/tokenize.rs
+++ projects/al_compiler/src/syntax/tokenize.rs
@@ -1,20 +1,35 @@
 use crate::syntax::*;
 use std::cell::Cell;
 
-struct Tokenizer<'a> {
-    file: usize,
+trait TokenKindTrait: Sized {
+    fn error() -> Self;
+
+    fn eof() -> Option<Self>;
+
+    fn is_trivia(&self) -> bool;
+}
+
+trait TokenFactory: Sized {
+    type Token;
+    type Kind: TokenKindTrait;
+
+    fn new_token(&self, kind: Self::Kind, start: usize, end: usize) -> Self::Token;
+}
+
+struct Tokenizer<'a, T: TokenFactory> {
+    token_factory: T,
     text: &'a str,
     last: usize,
     current: usize,
-    tokens: Vec<Token>,
+    tokens: Vec<T::Token>,
     tick: Cell<usize>,
     stuck: bool,
 }
 
-impl<'a> Tokenizer<'a> {
-    fn new(file: usize, text: &'a str) -> Self {
+impl<'a, T: TokenFactory> Tokenizer<'a, T> {
+    fn new(token_factory: T, text: &'a str) -> Self {
         Tokenizer {
-            file,
+            token_factory,
             text,
             last: 0,
             current: 0,
@@ -33,14 +48,12 @@ impl<'a> Tokenizer<'a> {
         self.current += self.text[self.current..].chars().next().unwrap().len_utf8();
     }
 
-    fn add_token(&mut self, kind: TokenKind) {
+    fn add_token(&mut self, kind: T::Kind) {
         assert!(self.last <= self.current && self.current <= self.text.len());
 
-        let text = self.current_text().to_string();
-        let location = SourceLocation::new(self.file, self.last, self.current);
-
-        if kind != TokenKind::Tombstone {
-            self.tokens.push(Token::new(kind, text, location));
+        if !T::Kind::is_trivia(&kind) {
+            let token = self.token_factory.new_token(kind, self.last, self.current);
+            self.tokens.push(token);
         }
 
         self.last = self.current;
@@ -93,16 +106,18 @@ impl<'a> Tokenizer<'a> {
     fn error(&mut self) {
         if self.stuck {
             self.bump();
-            self.add_token(TokenKind::Error);
+            self.add_token(T::Kind::error());
         }
         self.stuck = true;
     }
 
     fn eof(&mut self) {
-        self.add_token(TokenKind::Eof);
+        if let Some(kind) = T::Kind::eof() {
+            self.add_token(kind);
+        }
     }
 
-    fn tokenize(mut self) -> Vec<Token> {
+    fn tokenize(mut self, any: impl Fn(&mut Self)) -> Vec<T::Token> {
         while !self.at_eof() {
             any(&mut self);
             self.error();
@@ -112,26 +127,64 @@ impl<'a> Tokenizer<'a> {
     }
 }
 
-fn spaces(t: &mut Tokenizer<'_>) {
+impl TokenKindTrait for TokenKind {
+    fn error() -> Self {
+        TokenKind::Error
+    }
+
+    fn eof() -> Option<Self> {
+        Some(TokenKind::Eof)
+    }
+
+    fn is_trivia(&self) -> bool {
+        *self == TokenKind::Tombstone
+    }
+}
+
+struct AlteryTokenFactory<'a> {
+    file: usize,
+    text: &'a str,
+}
+
+impl<'a> AlteryTokenFactory<'a> {
+    fn new(file: usize, text :&'a str) -> Self {
+        AlteryTokenFactory {file,text}
+    }
+}
+
+impl<'a> TokenFactory for AlteryTokenFactory<'a> {
+    type Token = Token;
+    type Kind = TokenKind;
+
+    fn new_token(&self, kind: TokenKind, start: usize, end: usize) -> Token {
+        let text = self.text[start..end].to_string();
+        let location = SourceLocation::new(self.file, start, end);
+        Token::new(kind, text, location)
+    }
+}
+
+type Tok<'a> = Tokenizer<'a, AlteryTokenFactory<'a>>;
+
+fn spaces(t: &mut Tok<'_>) {
     if t.eat_while(|c| c.is_ascii_whitespace()) {
         t.add_token(TokenKind::Tombstone);
     }
 }
 
-fn ident(t: &mut Tokenizer<'_>) {
+fn ident(t: &mut Tok<'_>) {
     if !t.next().is_ascii_digit() && t.eat_while(|c| c.is_ascii_alphanumeric() || c == b'_') {
         let kind = ident_kind(t.current_text());
         t.add_token(kind);
     }
 }
 
-fn int(t: &mut Tokenizer<'_>) {
+fn int(t: &mut Tok<'_>) {
     if t.eat_while(|c| c.is_ascii_digit()) {
         t.add_token(TokenKind::Int);
     }
 }
 
-fn symbol(t: &mut Tokenizer<'_>) {
+fn symbol(t: &mut Tok<'_>) {
     for &(kind, text) in TokenKind::symbol_texts() {
         if t.eat(text) {
             t.add_token(kind);
@@ -139,7 +192,7 @@ fn symbol(t: &mut Tokenizer<'_>) {
     }
 }
 
-fn any(t: &mut Tokenizer<'_>) {
+fn any(t: &mut Tok<'_>) {
     spaces(t);
     ident(t);
     int(t);
@@ -155,6 +208,7 @@ fn ident_kind(text: &str) -> TokenKind {
     TokenKind::Ident
 }
 
-pub(crate) fn tokenize(file: usize, source: &str) -> Vec<Token> {
-    Tokenizer::new(file, source).tokenize()
+pub(crate) fn tokenize(file: usize, text: &str) -> Vec<Token> {
+    let token_factory = AlteryTokenFactory::new(file, text);
+    Tokenizer::new(token_factory, text).tokenize(any)
 }

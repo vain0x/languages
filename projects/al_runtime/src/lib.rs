9551b053b019dfed4d9f0aee8a71afc23b42ca5b
vain0x <vainzerox@gmail.com>
2019-07-14 00:17:15 +0900

    feat: セルを自動で脱参照する

--- projects/al_runtime/src/lib.rs
+++ projects/al_runtime/src/lib.rs
@@ -3,7 +3,16 @@ use crate::ins::*;
 pub(crate) mod il;
 pub(crate) mod ins;
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub(crate) enum CellTy {
+    None,
+    Bool,
+    Int,
+    Cell,
+}
+
 pub(crate) struct Runtime {
+    table_tys: Vec<CellTy>,
     table: Vec<i64>,
     heap_end: usize,
     stack_end: usize,
@@ -14,12 +23,22 @@ pub(crate) struct Runtime {
 impl Runtime {
     fn new() -> Self {
         Runtime {
+            table_tys: vec![CellTy::None; 1024],
             table: vec![0xcd; 1024],
             heap_end: 0,
             stack_end: 1024,
             inss: vec![],
             pc: 0,
         }
+    }
+
+    fn table_get(&self, addr: usize) -> (CellTy, i64) {
+        (self.table_tys[addr], self.table[addr])
+    }
+
+    fn table_set(&mut self, addr: usize, pair: (CellTy, i64)) {
+        self.table_tys[addr] = pair.0;
+        self.table[addr] = pair.1;
     }
 
     fn heap_alloc(&mut self, count: usize) -> (usize, usize) {
@@ -29,14 +48,23 @@ impl Runtime {
         (start, end)
     }
 
-    fn stack_push(&mut self, value: i64) {
+    fn stack_push(&mut self, (ty, value): (CellTy, i64)) {
         self.stack_end -= 1;
-        self.table[self.stack_end] = value;
+        self.table_set(self.stack_end, (ty, value));
     }
 
-    fn stack_pop(&mut self) -> i64 {
+    fn stack_pop(&mut self) -> (CellTy, i64) {
         self.stack_end += 1;
-        self.table[self.stack_end - 1]
+        self.table_get(self.stack_end - 1)
+    }
+
+    fn stack_pop_val(&mut self) -> (CellTy, i64) {
+        match self.stack_pop() {
+            (CellTy::Cell, addr) => {
+                self.table_get(addr as usize)
+            }
+            (ty, value) => (ty, value)
+        }
     }
 
     fn run(&mut self) -> ! {
@@ -45,51 +73,74 @@ impl Runtime {
             match self.inss[self.pc - 1] {
                 InsKind::Exit => std::process::exit(0),
                 InsKind::Assert => {
-                    if self.stack_pop() == 0 {
+                    if self.stack_pop() != (CellTy::Bool, 1) {
                         eprintln!("assertion error");
                         std::process::abort()
                     }
                 }
                 InsKind::CellSet => {
                     let right = self.stack_pop();
-                    let left = self.stack_pop() as usize;
-                    self.table[left] = right;
+                    let left = match self.stack_pop() {
+                        (CellTy::Cell, id) => id as usize,
+                        _ => panic!("Type error"),
+                    };
+                    self.table_set(left, right);
                 }
                 InsKind::Bool(value) => {
-                    self.stack_push(if value { 1 } else { 0 });
+                    self.stack_push((CellTy::Bool, value as i64));
                 }
                 InsKind::Int(value) => {
-                    self.stack_push(value);
+                    self.stack_push((CellTy::Int, value));
                 }
                 InsKind::GlobalGet => {
-                    let global_id = self.stack_pop();
-                    self.stack_push(global_id);
+                    match self.stack_pop() {
+                        (CellTy::Int, addr) => {
+                            self.stack_push((CellTy::Cell, addr));
+                        }
+                        _ => panic!("Type error"),
+                    }
                 }
                 InsKind::OpAdd => {
-                    let right = self.stack_pop();
-                    let left = self.stack_pop();
-                    self.stack_push(left + right);
+                    let right = self.stack_pop_val();
+                    let left = self.stack_pop_val();
+                    match (left, right) {
+                        ((CellTy::Int, left), (CellTy::Int, right)) => self.stack_push((CellTy::Int, left + right)),
+                        _ => panic!("Type error")
+                    }
                 }
                 InsKind::OpSub => {
-                    let right = self.stack_pop();
-                    let left = self.stack_pop();
-                    self.stack_push(left - right);
+                    let right = self.stack_pop_val();
+                    let left = self.stack_pop_val();
+                    match (left, right) {
+                        ((CellTy::Int, left), (CellTy::Int, right)) => self.stack_push((CellTy::Int, left - right)),
+                        _ => panic!("Type error")
+                    }
                 }
                 InsKind::OpMul => {
-                    let right = self.stack_pop();
-                    let left = self.stack_pop();
-                    self.stack_push(left * right);
+                    let right = self.stack_pop_val();
+                    let left = self.stack_pop_val();
+                    match (left, right) {
+                        ((CellTy::Int, left), (CellTy::Int, right)) => self.stack_push((CellTy::Int, left * right)),
+                        _ => panic!("Type error")
+                    }
                 }
                 InsKind::OpDiv => {
-                    let right = self.stack_pop();
-                    let left = self.stack_pop();
+                    let right = self.stack_pop_val();
+                    let left = self.stack_pop_val();
                     // FIXME: ゼロ除算をエラーにする
-                    self.stack_push(left / right);
+                    match (left, right) {
+                        ((CellTy::Int, left), (CellTy::Int, right)) => self.stack_push((CellTy::Int, left / right)),
+                        _ => panic!("Type error")
+                    }
                 }
                 InsKind::OpEq => {
-                    let right = self.stack_pop();
-                    let left = self.stack_pop();
-                    self.stack_push(if left == right { 1 } else { 0 });
+                    let right = self.stack_pop_val();
+                    let left = self.stack_pop_val();
+                    match (left, right) {
+                        ((CellTy::Bool, left), (CellTy::Bool, right)) => self.stack_push((CellTy::Bool, (left == right) as i64)),
+                        ((CellTy::Int, left), (CellTy::Int, right)) => self.stack_push((CellTy::Bool, (left == right) as i64)),
+                        _ => self.stack_push((CellTy::Bool, false as i64)),
+                    }
                 }
             }
         }

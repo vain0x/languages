ca0ab7ef0197dec96b93b9dd9f5542df0b017af6
vain0x <vainzerox@gmail.com>
2019-07-14 21:32:45 +0900

    refactor: IL 命令を常に帰りがけで解釈する
    すべての「引数」を再帰的に処理した後に命令に基づくコード生成を行う。
    例えば (root (f 1 (g 2) 3)) は (root 1 2 g 3 f) と同じ意味。
    
    識別子の扱いは命令の種類によって異なるので、単にスタックに積んでおき、
    命令がスタックから必要な数の識別子を取り出して処理する。

--- projects/al_runtime/src/al_asm/assemble.rs
+++ projects/al_runtime/src/al_asm/assemble.rs
@@ -3,105 +3,42 @@
 use super::*;
 use al_aux::il::*;
 
-fn gen_globals(il: usize, t: &IlTree, a: &mut AlAsm) {
+fn gen_il(il: usize, t: &IlTree, a: &mut AlAsm, idents: &mut Vec<usize>) {
     for ci in 0..t.child_len(il) {
-        let child = t.child(il, ci);
-        match t.kind(child) {
-            IlKind::Ident(string_id) => {
-                let ident = t.get_string(string_id).to_owned();
+        gen_il(t.child(il, ci), t, a, idents);
+    }
+
+    match t.kind(il) {
+        // 宣言:
+        IlKind::Globals => {
+            // (globals $a ..) の中身がすべてスタックに積まれているので、それらをグローバル変数として宣言する。
+            for ident in 0..idents.len() {
+                let ident = t.get_string(ident).to_owned();
                 a.globals.new_global(ident);
             }
-            kind => unreachable!(
-                "globals の子ノードはすべて識別子でなければいけない {:?}",
-                kind
-            ),
+            idents.clear();
         }
-    }
-}
-
-fn gen_labels(il: usize, t: &IlTree, a: &mut AlAsm) {
-    for ci in 0..t.child_len(il) {
-        let child = t.child(il, ci);
-        match t.kind(child) {
-            IlKind::Ident(string_id) => {
-                let ident = t.get_string(string_id).to_owned();
+        IlKind::Labels => {
+            // globals と同様
+            for ident in 0..idents.len() {
+                let ident = t.get_string(ident).to_owned();
                 a.labels.new_label(ident);
             }
-            kind => unreachable!(
-                "labels の子ノードはすべて識別子でなければいけない {:?}",
-                kind
-            ),
+            idents.clear();
         }
-    }
-}
-
-fn gen_label_def(il: usize, t: &IlTree, a: &mut AlAsm) {
-    assert_eq!(t.child_len(il), 1);
-    match t.kind(t.child(il, 0)) {
-        IlKind::Ident(string_id) => {
-            let ident = t.get_string(string_id);
-            let label = a.labels.find(ident).expect("ラベルは定義済みのはず");
+        IlKind::LabelDef => {
+            let ident = idents.pop().expect("label_def は識別子を1つ持つはず");
+            let ident = t.get_string(ident);
+            let label = a.labels.find(ident).unwrap_or_else(|| {
+                panic!(
+                    "labels に列挙されていないラベルがあります {}",
+                    ident
+                )
+            });
             a.labels.def_label(label, a.instrs.len());
         }
-        kind => unreachable!(
-            "label_def の子ノードは識別子でなければいけない {:?}",
-            kind
-        ),
-    }
-}
 
-fn gen_global_get(il: usize, t: &IlTree, a: &mut AlAsm) {
-    assert_eq!(t.child_len(il), 1);
-    match t.kind(t.child(il, 0)) {
-        IlKind::Ident(string_id) => {
-            let ident = t.get_string(string_id);
-            let global_id = a
-                .globals
-                .find(ident)
-                .expect("グローバル変数は定義済みのはず");
-            a.new_instr(InstrKind::Int(global_id as i64));
-            a.new_instr(InstrKind::GlobalGet);
-        }
-        kind => unreachable!(
-            "global_get の子ノードは識別子でなければいけない {:?}",
-            kind
-        ),
-    }
-}
-
-fn gen_label_get(il: usize, t: &IlTree, a: &mut AlAsm) {
-    assert_eq!(t.child_len(il), 1);
-    match t.kind(t.child(il, 0)) {
-        IlKind::Ident(string_id) => {
-            let ident = t.get_string(string_id);
-            let label = a.labels.find(ident).expect("ラベルは定義済みのはず");
-            a.labels.use_label(label, a.instrs.len());
-            a.new_instr(InstrKind::Label(label));
-        }
-        kind => unreachable!(
-            "label_get の子ノードは識別子でなければいけない {:?}",
-            kind
-        ),
-    }
-}
-
-fn gen_il(il: usize, t: &IlTree, a: &mut AlAsm) {
-    match t.kind(il) {
-        IlKind::Globals => return gen_globals(il, t, a),
-        IlKind::Labels => return gen_labels(il, t, a),
-        IlKind::LabelDef => return gen_label_def(il, t, a),
-        IlKind::GlobalGet => return gen_global_get(il, t, a),
-        IlKind::LabelGet => return gen_label_get(il, t, a),
-        _ => {}
-    }
-
-    for ci in 0..t.child_len(il) {
-        gen_il(t.child(il, ci), t, a);
-    }
-
-    match t.kind(il) {
         // 文:
-        IlKind::Semi => {}
         IlKind::Jump => a.new_instr(InstrKind::Jump),
         IlKind::JumpUnless => a.new_instr(InstrKind::JumpUnless),
         IlKind::Pop => a.new_instr(InstrKind::Pop),
@@ -111,6 +48,32 @@ fn gen_il(il: usize, t: &IlTree, a: &mut AlAsm) {
         // 式:
         IlKind::Bool(value) => a.new_instr(InstrKind::Bool(value)),
         IlKind::Int(value) => a.new_instr(InstrKind::Int(value)),
+
+        IlKind::GlobalGet => {
+            let ident = idents.pop().expect("global_get は識別子を1つ持つはず");
+            let ident = t.get_string(ident);
+            let global_id = a.globals.find(ident).unwrap_or_else(|| {
+                panic!(
+                    "globals に列挙されていないグローバル変数があります {}",
+                    ident
+                )
+            });
+            a.new_instr(InstrKind::Int(global_id as i64));
+            a.new_instr(InstrKind::GlobalGet);
+        }
+        IlKind::LabelGet => {
+            let ident = idents.pop().expect("label_get は識別子を1つ持つはず");
+            let ident = t.get_string(ident);
+            let label = a.labels.find(ident).unwrap_or_else(|| {
+                panic!(
+                    "labels に列挙されていないラベルがあります {}",
+                    ident
+                )
+            });
+            a.labels.use_label(label, a.instrs.len());
+            a.new_instr(InstrKind::Label(label));
+        }
+
         IlKind::OpAdd => a.new_instr(InstrKind::OpAdd),
         IlKind::OpSub => a.new_instr(InstrKind::OpSub),
         IlKind::OpMul => a.new_instr(InstrKind::OpMul),
@@ -118,28 +81,17 @@ fn gen_il(il: usize, t: &IlTree, a: &mut AlAsm) {
         IlKind::OpEq => a.new_instr(InstrKind::OpEq),
 
         // その他:
-        IlKind::Ident(string_id) => {
-            let ident = t.get_string(string_id);
-            match a.globals.find(ident) {
-                Some(global_id) => a.new_instr(InstrKind::Int(global_id as i64)),
-                None => panic!("Unknown ident {}", ident),
-            }
-        }
+        IlKind::Ident(ident_id) => idents.push(ident_id),
 
-        IlKind::Root
-        | IlKind::CodeSection
-        | IlKind::Globals
-        | IlKind::Labels
-        | IlKind::LabelDef
-        | IlKind::GlobalGet
-        | IlKind::LabelGet => {}
+        IlKind::Root | IlKind::CodeSection | IlKind::Semi => {}
     }
 }
 
 pub(crate) fn assemble(t: &IlTree) -> AlAsm {
     let mut a = AlAsm::new();
+    let mut idents = vec![];
 
-    gen_il(t.root(), t, &mut a);
+    gen_il(t.root(), t, &mut a, &mut idents);
     a.instrs.push(InstrKind::Exit);
 
     a.resolve_labels();

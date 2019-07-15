ace47488ce8b1e0300f7bce572dbb68480631bc5
vain0x <vainzerox@gmail.com>
2019-07-15 11:05:07 +0900

    feat: 宣言の正準化を実装

--- projects/al_compiler/src/semantics/canon.rs
+++ projects/al_compiler/src/semantics/canon.rs
@@ -1,6 +1,6 @@
 //! 正準化 (canonicalize)
 //!
-//! 式を項と文に分類する。
+//! 式を項と文と宣言に分類する。
 //!
 //! semi 文の直下をすべて文になるように変形する。
 //! 例えば、semi 文の直下にある項 (式文) は do 文に変形する。
@@ -26,7 +26,7 @@ fn canon_term(expr: &mut Expr, stmts: &mut Vec<Expr>) {
 }
 
 /// 式を文とみなして正準化する。
-fn canon_stmt(mut expr: Expr, stmts: &mut Vec<Expr>) {
+fn canon_stmt(mut expr: Expr, stmts: &mut Vec<Expr>, decls: &mut Vec<Expr>) {
     match expr.kind() {
         ExprKind::Lit(..)
         | ExprKind::Prim(..)
@@ -47,8 +47,8 @@ fn canon_stmt(mut expr: Expr, stmts: &mut Vec<Expr>) {
             match expr.children_mut().as_mut_slice() {
                 [cond, body, alt] => {
                     canon_term(cond, stmts);
-                    canon_block(body);
-                    canon_block(alt);
+                    canon_block(body, decls);
+                    canon_block(alt, decls);
                 }
                 _ => unimplemented!("no else yet"),
             }
@@ -56,21 +56,21 @@ fn canon_stmt(mut expr: Expr, stmts: &mut Vec<Expr>) {
         }
         ExprKind::Semi => {
             for child in expr.children_mut().drain(..) {
-                canon_stmt(child, stmts)
+                canon_stmt(child, stmts, decls)
             }
         }
     }
 }
 
-/// 式をブロックとして正準化する。
+/// 式をブロックとみなして正準化する。
 /// 式の内部に含まれる複数の文からなる1個の semi 文に式を変換する。
 /// 文の子要素はすべて文であり、文の孫要素は文または項であり、項の子孫要素はすべて項である。
-fn canon_block(expr: &mut Expr) {
+fn canon_block(expr: &mut Expr, decls: &mut Vec<Expr>) {
     let (main_loc, total_loc) = (*expr.main_loc(), *expr.total_loc());
     let semi = Expr::new(ExprKind::Semi, vec![], main_loc, total_loc);
 
     let mut stmts = vec![];
-    canon_stmt(replace(expr, semi), &mut stmts);
+    canon_stmt(replace(expr, semi), &mut stmts, decls);
 
     expr.children_mut().extend(stmts);
 
@@ -81,6 +81,27 @@ fn canon_block(expr: &mut Expr) {
     }
 }
 
+/// 式を宣言とみなして正準化する。
+/// トップレベルは semi 宣言であり、直下に宣言または文が並ぶ。
+fn canon_decl(expr: &mut Expr) {
+    let (main_loc, total_loc) = (*expr.main_loc(), *expr.total_loc());
+    let mut semi = Expr::new(ExprKind::Semi, vec![], main_loc, total_loc);
+
+    let mut decls = vec![];
+    canon_block(expr, &mut decls);
+
+    semi.children_mut().extend(decls);
+
+    let last = replace(expr, semi);
+    expr.children_mut().push(last);
+
+    // 1要素の semi 文を unwrap する。
+    if expr.children().len() == 1 {
+        let inner = expr.children_mut().drain(..).next().unwrap();
+        *expr = inner;
+    }
+}
+
 pub(crate) fn canon(expr: &mut Expr) {
-    canon_block(expr);
+    canon_decl(expr);
 }

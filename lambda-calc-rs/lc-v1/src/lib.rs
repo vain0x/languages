b1d8af499fe43fa18e39de291bbfefae2cbc6364
vain0x <vainzerox@gmail.com>
2020-09-04 15:40:07 +0900

    feat: expect_test を試す

--- lambda-calc-rs/lc-v1/src/lib.rs
+++ lambda-calc-rs/lc-v1/src/lib.rs
@@ -1,7 +1,16 @@
 #[cfg(test)]
 mod tests {
+    use expect_test::expect;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn snapshot_test() {
+        let actual = "OK!";
+        let expect = expect![[r#"OK!"#]];
+        expect.assert_eq(actual);
     }
 }

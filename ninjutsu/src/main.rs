12e1cd47b685191bc86c948ea5197ca9f5f9c52a
vain0x <vainzerox@gmail.com>
2022-02-06 00:14:50 +0900

    feat: Tokenize with logos

---
+++ ninjutsu/src/main.rs
@@ -0,0 +1,37 @@
+use ninjutsu::tokenize;
+use std::{env::args, fs, path::PathBuf, process::exit};
+
+fn help() -> ! {
+    println!("ninjutsu <SUBCOMMAND>");
+    exit(0)
+}
+
+fn main() {
+    let mut args = args();
+    args.next();
+
+    match args.next().as_deref().unwrap_or("--help") {
+        "--help" | "-h" | "help" => help(),
+
+        "tokenize" => {
+            for filename in args {
+                let filename = PathBuf::from(filename);
+                let buf = fs::read_to_string(&filename).expect("read");
+                let tokens = tokenize::tokenize_to_vec(&buf);
+                let w = fs::OpenOptions::new()
+                    .create(true)
+                    .truncate(true)
+                    .write(true)
+                    .open(filename.with_extension("nin_tokens"))
+                    .expect("write");
+                let mut serializer = serde_json::Serializer::new(w);
+                serde::Serialize::serialize(&tokens, &mut serializer).expect("serialize");
+            }
+        }
+
+        arg => {
+            eprintln!("Unknown subcommand '{}'.", arg);
+            exit(1)
+        }
+    }
+}
